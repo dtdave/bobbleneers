abea43c620e9d4d22dc30800589a9b8fcc4aee24
Shawn Pearson <1157wcl@gmail.com>
2023-08-23 15:44:21 -0400

    oAuth working

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-//const express = require("express");
 const axios = require("axios").default;
 const crypto = require("crypto");
-//const app = express();
-
+const querystring = require("querystring");
+if (!process.env.tenantUri) {
+  console.log("Warning!  Variables not set!");
+}
 const uri = "https://" + process.env.tenantUri + "/api/v2/logs/ingest";
 const token = process.env.tenantToken;
-console.log("uri: ", uri);
-console.log("token", token);
+const oAuthClientId = process.env.clientId;
+const oAuthSecret = process.env.clientSecret;
+var oAuthCredentials; // oAuth creds
+var oAuthTimer = new Date(); // set a timer for refreshing oAuth token
+console.log("startup!");
+function setTimer(minutes) {
+  rightNow = new Date();
+  // console.log(rightNow);
+  return new Date(rightNow.getTime() + minutes * 60000);
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -16,131 +25,128 @@ function getRandomInt(min, max) {
 
 const headers = {
   accept: "application/json; charset=utf-8",
-  "Content-Type": "application/json; charset=utf-8",
+  "Content-type": "application/json; charset=utf-8",
   Authorization: "Api-Token " + token,
 };
 
-const newPost = async () => {
-  const logIngest = [];
-  const names = [
-    "Richard L. Lambert",
-    "Robert E. Williams",
-    "Troy S. Igney",
-    "David Manes",
-    "Gary Carr",
-    "Michael Hoernemann",
-    "Anthony Pope-cruz",
-    "Matthew Caminiti",
-    "Jaime A. Chavez Barragan",
-    "Alan D. Paoloni",
-    "Alejandro Nunez Valdivia",
-    "Francisco Merino Menares",
-    "Uriel S. Ramirez Castaño",
-    "Andrea Calderon",
-    "Alejandro Mercado",
-    "Carlos A. Fernandez Garcia",
-    "Angel Aleman Santos",
-    "Jorge Ricardo Rea Santiago",
-    "Jose Angel Estrada Sanchez",
-    "Raymundo Nunez Pena",
-    "Ricardo J. Julia Diaz",
-    "Handerson Medeiros",
-    "Andrey R. Intima",
-    "Alan R. Delcaro",
-    "Alexandre Sacco",
-    "Francisco Merino Menares",
-    "Igor Simoes",
-    "Gabriel Marques",
-    "Willian Rodrigues",
-    "Marcelo B. Coletta",
-    "John Rath",
-    "Thomas Lagona",
-    "Nathan Mcclain",
-    "Daniel Petreyko",
-    "Kevin M. Colton",
-    "David E. Thomas",
-    "Shawn Pearson",
-    "Richard St. John",
-    "Michael Ferguson",
-    "Jerry Lobenstein",
-    "Michael S. Ditmar",
-    "Robert Stojan",
-    "Joshua Wood",
-    "Jeffrey Neau",
-    "Lawrence Cuneaz",
-    "Nathan Kunz",
-    "Sean M. Feeney",
-    "Cooper J. Fecteau",
-    "Devon Mitchell",
-    "Jigar Pithwa",
-    "Pravin Dandra",
-    "Michael Sarbacker",
-    "David Rupert",
-    "Timothy J. Finkbeiner",
-    "Keith Ellis",
-    "Cynthia L. Harper",
-    "Kirk Sievers",
-    "Eric Monterastelli",
-    "Rodrigo Alvarez Seoane",
-    "Matthew S. Gardner",
-    "Willie F. Hicks Jr.",
-    "Susan R. StClair",
-    "Carl Audet",
-    "David Jones",
-    "Kyle Harrington",
-    "Shivashish Pati",
-    "Ryan Shaw",
-    "Joseph Nguyen",
-    "Jason Bortolotti",
-    "Brendan Steiner",
-    "Michael Taylor",
-    "Paul D. Brugan",
-    "Arun Gopal",
-    "James Conrad",
-    "John DeBlasio",
-    "Scott Zillitto",
-    "Phillip Rand",
-    "William Abbott",
-    "Douglas Pak",
-    "Alexander R. Brewers",
-    "Benjamin W. Styx",
-    "Jacob Norkus",
-    "Nicoe Welch",
-    "Santiago F. Palacios Garcia",
-    "Aditya Ganesh",
-    "Sara Al-Hayale",
-    "Andrew Chasin",
-    "Markie Duby",
-    "Arihant Polavarapu",
-    "Edin D. Cristofi",
-    "Justin Phelps",
-    "Aakash Kadakia",
-    "Stephen Schleh",
-    "Kendon Cowart",
-    "Elijah W. Gillis-Watson",
-    "Mateusz Gibiec",
-    "Brian Chandler",
-    "Jason Ostroski",
-    "Joseph Bologna",
-    "Eric Eiswerth",
-    "Courtney D. Sparks",
-    "Jeffery Yarbrough",
-    "Timothy E. Yara",
-    "Thomas J. Clinkenbeard",
-    "Nish Rama",
-    "Tory Clark",
-    "Robin Beran",
-    "Robert J. Vogler",
-    "Kyle Kowalski",
-    "Jacob R. Crawford",
-    "Gregory Speckhart",
-    "Jeffery Fynboh",
-    "Brian J. Wilson",
-  ];
-  const customerName = names[Math.floor(Math.random() * names.length)];
-  const productOrdered = names[Math.floor(Math.random() * names.length)];
-  const productNickName = productOrdered.split(" ")[0];
-  const goodFeedback = [
+const names = [
+  "Richard L. Lambert",
+  "Robert E. Williams",
+  "Troy S. Igney",
+  "David Manes",
+  "Gary Carr",
+  "Michael Hoernemann",
+  "Anthony Pope-cruz",
+  "Matthew Caminiti",
+  "Jaime A. Chavez Barragan",
+  "Alan D. Paoloni",
+  "Alejandro Nunez Valdivia",
+  "Francisco Merino Menares",
+  "Uriel S. Ramirez Castaño",
+  "Andrea Calderon",
+  "Alejandro Mercado",
+  "Carlos A. Fernandez Garcia",
+  "Angel Aleman Santos",
+  "Jorge Ricardo Rea Santiago",
+  "Jose Angel Estrada Sanchez",
+  "Raymundo Nunez Pena",
+  "Ricardo J. Julia Diaz",
+  "Handerson Medeiros",
+  "Andrey R. Intima",
+  "Alan R. Delcaro",
+  "Alexandre Sacco",
+  "Francisco Merino Menares",
+  "Igor Simoes",
+  "Gabriel Marques",
+  "Willian Rodrigues",
+  "Marcelo B. Coletta",
+  "John Rath",
+  "Thomas Lagona",
+  "Nathan Mcclain",
+  "Daniel Petreyko",
+  "Kevin M. Colton",
+  "David E. Thomas",
+  "Shawn Pearson",
+  "Richard St. John",
+  "Michael Ferguson",
+  "Jerry Lobenstein",
+  "Michael S. Ditmar",
+  "Robert Stojan",
+  "Joshua Wood",
+  "Jeffrey Neau",
+  "Lawrence Cuneaz",
+  "Nathan Kunz",
+  "Sean M. Feeney",
+  "Cooper J. Fecteau",
+  "Devon Mitchell",
+  "Jigar Pithwa",
+  "Pravin Dandra",
+  "Michael Sarbacker",
+  "David Rupert",
+  "Timothy J. Finkbeiner",
+  "Keith Ellis",
+  "Cynthia L. Harper",
+  "Kirk Sievers",
+  "Eric Monterastelli",
+  "Rodrigo Alvarez Seoane",
+  "Matthew S. Gardner",
+  "Willie F. Hicks Jr.",
+  "Susan R. StClair",
+  "Carl Audet",
+  "David Jones",
+  "Kyle Harrington",
+  "Shivashish Pati",
+  "Ryan Shaw",
+  "Joseph Nguyen",
+  "Jason Bortolotti",
+  "Brendan Steiner",
+  "Michael Taylor",
+  "Paul D. Brugan",
+  "Arun Gopal",
+  "James Conrad",
+  "John DeBlasio",
+  "Scott Zillitto",
+  "Phillip Rand",
+  "William Abbott",
+  "Douglas Pak",
+  "Alexander R. Brewers",
+  "Benjamin W. Styx",
+  "Jacob Norkus",
+  "Nicoe Welch",
+  "Santiago F. Palacios Garcia",
+  "Aditya Ganesh",
+  "Sara Al-Hayale",
+  "Andrew Chasin",
+  "Markie Duby",
+  "Arihant Polavarapu",
+  "Edin D. Cristofi",
+  "Justin Phelps",
+  "Aakash Kadakia",
+  "Stephen Schleh",
+  "Kendon Cowart",
+  "Elijah W. Gillis-Watson",
+  "Mateusz Gibiec",
+  "Brian Chandler",
+  "Jason Ostroski",
+  "Joseph Bologna",
+  "Eric Eiswerth",
+  "Courtney D. Sparks",
+  "Jeffery Yarbrough",
+  "Timothy E. Yara",
+  "Thomas J. Clinkenbeard",
+  "Nish Rama",
+  "Tory Clark",
+  "Robin Beran",
+  "Robert J. Vogler",
+  "Kyle Kowalski",
+  "Jacob R. Crawford",
+  "Gregory Speckhart",
+  "Jeffery Fynboh",
+  "Brian J. Wilson",
+];
+
+function getGoodFeedback(productNickName) {
+  const feedback = [
     "Product is awesome, shipped as advertised!",
     "Great product, shipped fast",
     "High quality- my kids are enormous fans of " + productNickName + "!",
@@ -154,24 +160,25 @@ const newPost = async () => {
       customerName +
       "!'  every time I write an email.",
   ];
-  const badFeedback = [
+  review = feedback[Math.floor(Math.random() * feedback.length)];
+  return review;
+}
+
+function getBadFeedback(productNickName) {
+  const feedback = [
     "LightningBobble, more like Lame-o-Bobble.  Took forever to arrive.",
-    "I purchased " + productOrdered + " for my niece's birthday.  Arrived 4 days later- she was devastated.",
+    "I purchased " + productNickName + " for my niece's birthday.  Arrived 4 days later- she was devastated.",
     "YOU ALL SUCK.  REFUNDED!!ONEONE!!ONE!",
     "Wow, way to ruin my week.  I told everyone at work I'd bring in " +
-      productOrdered +
+      productNickName +
       " to show them, but I still haven't got it yet.  Company did refund shipping as promised.",
     "Confidence totally shaken in an otherwise outstanding company.  Their new shipping service is awful.  Immediately requested shipping refund.",
   ];
-  const customeruid = crypto.randomUUID();
-  const orderid = crypto.randomUUID();
-  console.log("order id: ", orderid);
-  const shipDelay = getRandomInt(18, 28);
-  const logDate = new Date();
-  const dateModifier = getRandomInt(0, 14);
-  const baseDate = new Date(new Date(logDate).setDate(logDate.getDate() - dateModifier));
-  const baseDay = baseDate.getDay();
-  var mondayDelay, warehouseLocation, shipTime, deliveryTime, distVersion, lightningStatus, content, review, rating;
+  review = feedback[Math.floor(Math.random() * feedback.length)];
+  return review;
+}
+
+function dayDelay(baseDay) {
   switch (baseDay) {
     case 0:
       mondayDelay = 1;
@@ -195,6 +202,10 @@ const newPost = async () => {
       mondayDelay = 2;
       break;
   }
+  return mondayDelay;
+}
+
+function warehouse() {
   const scenarioNumber = getRandomInt(1, 7);
 
   switch (scenarioNumber) {
@@ -220,6 +231,25 @@ const newPost = async () => {
       warehouseLocation = "Oregon";
       break;
   }
+  return warehouseLocation;
+}
+
+const newPost = async () => {
+  const logIngest = [];
+  const customerName = names[Math.floor(Math.random() * names.length)];
+  const productOrdered = names[Math.floor(Math.random() * names.length)];
+  const productNickName = productOrdered.split(" ")[0];
+  const customeruid = crypto.randomUUID();
+  const orderid = crypto.randomUUID();
+  console.log("order id: ", orderid);
+  const shipDelay = getRandomInt(18, 28);
+  const logDate = new Date();
+  const dateModifier = getRandomInt(0, 14);
+  const baseDate = new Date(new Date(logDate).setDate(logDate.getDate() - dateModifier));
+  const baseDay = baseDate.getDay();
+  var mondayDelay, warehouseLocation, shipTime, deliveryTime, distVersion, lightningStatus, content, review, rating;
+  mondayDelay = dayDelay(baseDay);
+  warehouseLocation = warehouse();
 
   if (warehouseLocation == "Nevada") {
     //BAD scenario - likely too long and customer unhappy
@@ -239,11 +269,11 @@ const newPost = async () => {
 
   if (shipTime.getDate() - baseDate.getDate() > 2) {
     //Took too long to ship. customer angy.
-    review = badFeedback[Math.floor(Math.random() * badFeedback.length)];
+    review = getBadFeedback(productNickName);
     rating = 1;
   } else {
     //happy customer
-    review = goodFeedback[Math.floor(Math.random() * goodFeedback.length)];
+    review = getGoodFeedback(productNickName);
     rating = getRandomInt(3, 5);
   }
 
@@ -341,11 +371,50 @@ const sendPostRequest = async () => {
     console.log("successful response: ", resp.status);
   } catch (err) {
     // Handle Error Here
-    console.error(err);
+    console.error(err.response.status);
   }
 };
 
-setInterval(() => {
-  sendPostRequest();
-}, 2000);
+const sendBizEvents = async () => {
+  const currentDate = new Date();
+  //console.log("starting auth", oAuthCredentials);
+  //console.log(oAuthTimer);
+  const authTimeRemaining = (oAuthTimer.getTime() - currentDate.getTime()) / 1000;
+  if (authTimeRemaining < 60) {
+    console.log("Refreshing oAuth. Expiration in: ", authTimeRemaining, "s.");
+    await getAuth();
+  }
+  // console.log("final auth", oAuthCredentials);
+};
+
+const getAuth = async () => {
+  const oAuthHeaders = {
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  const data = {
+    grant_type: "client_credentials",
+    client_id: oAuthClientId,
+    client_secret: oAuthSecret,
+  };
+  const endPoint = "https://sso.dynatrace.com/sso/oauth2/token";
+  try {
+    const resp = await axios.post(endPoint, querystring.stringify(data), { headers: oAuthHeaders });
+    if (resp.status == 200) {
+      oAuthCredentials = resp.data.access_token;
+      oAuthTimer = setTimer(resp.data.expires_in);
+    } else {
+      console.log("Boo. I got non-200 response: ", resp.status);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  console.log("refreshed token.  Timer is: ", oAuthTimer);
+};
+
+// setInterval(() => {
+//   sendPostRequest();
+//
 // sendPostRequest();
+setInterval(() => {
+  sendBizEvents();
+}, 2000);
--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,23 @@
-$env:tenantUri = read-host -Prompt "Tenant"
-$env:tenantToken = read-host -Prompt "token"
+param(
+    [string] $url, # base API URL
+    [string] $token, # legacy token
+    [string] $clientId, #oAuth clientId
+    [string] $secret #oAuth Secret
+)
+
+if ($Matches) { Clear-Variable Matches }
+$url -match '\w{8}' | Out-Null
+if ($Matches) { $cleanTenantID = $Matches[0] }
+else { write-host "Tenant ID should be at least 8 alphanumeric characters." }
+if ($tenantID -like "*sprint*") {
+    # Sprint tenant
+    $validUrl = "$cleanTenantID.sprint.dynatracelabs.com"
+}
+else {
+    # Assumne live tenant
+    $validUrl = "$cleanTenantID.live.dynatrace.com"
+}
+$env:tenantUri = $validUrl
+$env:tenantToken = $token
+$env:clientId = $clientId
+$env:clientSecret = $secret
